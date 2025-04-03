cea8a536f456f2198f7885c753dca76da8916837
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-04-03 14:21:05 +0530

    add initial server implementation with Hono framework

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/ping', (c) => {
+  return c.json({pong: "Hello world!"})
+})
+
+app.get("/platform", (c) =>{
+  return c.json({platform: process.platform})
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
